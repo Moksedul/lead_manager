bfcbc6c72fb6f554c4b1c1d6fdb5c8d5d3cd62d5
Moksedul Alam <mokseduln@gmail.com>
2024-02-26 23:17:35 +0600

    home-pc commit

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 import json
 from datetime import date, timedelta
+from math import ceil
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -11,19 +12,47 @@ from django.views.decorators.http import require_POST
 from webpush import send_user_notification
 
 from leads.models import Lead
+from user.models import Team
 
 
 @login_required()
 def home(request):
-    leads = Lead.objects.filter(uploaded_time__range=[(date.today() - timedelta(days=30)), date.today()]).order_by('-id')[:15]
+    leads = Lead.objects.filter(uploaded_time__range=[(date.today() - timedelta(days=90)), date.today()]).order_by('-id')[:15]
     webpush_settings = getattr(settings, 'WEBPUSH_SETTINGS', {})
     vapid_key = webpush_settings.get('VAPID_PUBLIC_KEY')
     user = request.user
+
+    teams = Team.objects.all()
+
+    all_leads = Lead.objects.all()
+
+    leads_sold = all_leads.filter(status='Sold')
+
+    overall_progress = (len(leads_sold)/len(all_leads)) * 100
+
+    team_progress = []
+
+    for team in teams:
+        leader = team.team_leader
+        team_leads = all_leads.filter(assigned_to=leader)
+
+        lead_sold = team_leads.filter(status='Sold')
+        progress = (len(lead_sold)/len(team_leads)) * 100
+
+        data = {
+            'team': team.team_name,
+            'progress': ceil(progress),
+        }
+
+        team_progress.append(data)
+
     context = {
         'tittle': 'Lead Manager | Home',
         user: user,
         'vapid_key': vapid_key,
-        'leads': leads
+        'leads': leads,
+        'overall_progress': ceil(overall_progress),
+        'team_progress': team_progress,
     }
     return render(request, 'main/home.html', context)
 
